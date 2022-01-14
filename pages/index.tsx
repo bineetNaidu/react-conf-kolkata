9bf4414630c39a8d0d1f4c0a6df99b86090bf3f7
bineetNaidu <bineet.s.naidu@gmail.com>
2022-01-14 13:11:43 +0530

    🎉 create Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
@@ -7,9 +8,28 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | React Conf Kolkata</title>
       </Head>
+      <div className="container mx-auto px-4 py-10 text-center">
+        <h1 className="text-white text-8xl font-bold">
+          The first <span className="italic text-pink-800">React</span>{' '}
+          conference in <span className="italic text-pink-800">Kolkata</span> 🌴
+        </h1>
+        <p className="text-gray-500 text-3xl text-center w-3/4 mx-auto mt-8">
+          An in-person conference within eMerge Salt Lake, anchoring Kolkata
+          Tech Week.
+        </p>
 
-      <div className="min-h-screen h-full bg-black">
-        <h1 className="text-pink-300">Hello React Conf Kolkata</h1>
+        <h3 className="text-pink-700 mt-8 text-3xl mb-24">
+          APRIL 18 - 19, 2022 |
+          <span className="font-bold"> Technopolis, Salt Lake City</span>
+        </h3>
+
+        <div className="flex justify-center mt-8">
+          <Link href="/buy-tickets">
+            <a className="bg-pink-800 text-white font-bold py-2 px-4 rounded">
+              Tickets
+            </a>
+          </Link>
+        </div>
       </div>
     </>
   );
